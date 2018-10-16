e58e6dd295f0d5da0b85f1da4f561808aff87e6c
Gabriel Cretin <gabrielcretin@gmail.com>
2018-10-16 18:21:46 +0200

    Test dist func and print matrix

--- fold-U.py
+++ fold-U.py
@@ -4,15 +4,20 @@
 # IMPORTS
 import src.parsing as parse
 import src.threading as threading
+import numpy as np
 
 METAFOLD_FILE = "data/METAFOLD.list"
 FOLDREC_FILE = "data/foldrec/Agglutinin.foldrec"
 NB_TEMPLATES = 10
-DIST_RANGE = [5, 10] 
+DIST_RANGE = [5, 10]
 
 if __name__ == "__main__":
     metafold_dict = parse.metafold(METAFOLD_FILE)
     alignment_list = parse.foldrec(FOLDREC_FILE, NB_TEMPLATES, metafold_dict)
     for ali in alignment_list:
-        matrix = threading.calc_dist_matrix(ali.query_seq, ali.template.seq, DIST_RANGE)
-    print(matrix)
+        matrix = threading.calc_dist_matrix(ali.query_residues, ali.template.residues, DIST_RANGE)
+
+for i in range(20):
+    for j in range(20):
+        print(matrix[i][j], end=" ")
+    print("\n")
