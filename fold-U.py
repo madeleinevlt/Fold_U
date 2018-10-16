fc9d84af27a0e336f3bcc6e0b0a92e3f80a606a0
Gabriel Cretin <gabrielcretin@gmail.com>
2018-10-16 23:45:07 +0200

    little forgetfulness

--- fold-U.py
+++ fold-U.py
@@ -16,4 +16,4 @@ if __name__ == "__main__":
     alignment_list = parse.foldrec(FOLDREC_FILE, NB_TEMPLATES, metafold_dict)
     for ali in alignment_list:
         matrix = threading.calc_dist_matrix(ali.query_residues, ali.template.residues, DIST_RANGE)
-        threading.display_matrix(matrix, len(ali.query_residues))
+        threading.display_matrix(matrix)
