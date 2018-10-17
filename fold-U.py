d8596b1b4e97285b7aa8e5fd4521f08c3e399869
Gabriel Cretin <gabrielcretin@gmail.com>
2018-10-17 10:40:23 +0200

    Bug with Cohesin.foldrec

--- fold-U.py
+++ fold-U.py
@@ -7,14 +7,15 @@ import src.threading as threading
 import numpy as np
 
 METAFOLD_FILE = "data/METAFOLD.list"
-FOLDREC_FILE = "data/foldrec/Agglutinin.foldrec"
-NB_TEMPLATES = 1
+FOLDREC_FILE = "data/foldrec/Cohesin.foldrec"
+NB_TEMPLATES = 10
 DIST_RANGE = [5, 10]
 
 if __name__ == "__main__":
     metafold_dict = parse.metafold(METAFOLD_FILE)
     alignment_list = parse.foldrec(FOLDREC_FILE, NB_TEMPLATES, metafold_dict)
     for ali in alignment_list:
+        print(len(ali.query_residues), len(ali.template.residues))
         matrix = threading.calc_dist_matrix(
-            ali.query_residues[:40], ali.template.residues[:40], DIST_RANGE)
-        threading.display_matrix(matrix)
+            ali.query_residues, ali.template.residues, DIST_RANGE)
+        #threading.display_matrix(matrix)
