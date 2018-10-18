3834ddda160f39dd6b2191ec1aa6af8d5fab9d6c
Gabriel Cretin <gabrielcretin@gmail.com>
2018-10-18 23:34:18 +0200

    Write a small message in terminal when finished

--- src/writing.py
+++ src/writing.py
@@ -12,12 +12,13 @@ def scores(filename, score_list):
         name associated.
 
         Args:
-            filename (str): The path of the file the create
+            filename (str): The path of the file to create
             score_list (list): A list of tupples (score, template's name)
 
         Returns:
             void
     """
+    print("\nThe program ended successfully !\nThe results are stored in " + filename)
     with open(filename, "w") as file:
         for score, name in score_list:
             file.write("{:<5d}{}\n".format(int(score), name))
