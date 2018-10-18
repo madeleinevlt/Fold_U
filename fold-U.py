ea366812dfc5b212f7dc1dba543fac7a72910214
Gabriel Cretin <gabrielcretin@gmail.com>
2018-10-18 18:10:29 +0200

    test full script

--- fold-U.py
+++ fold-U.py
@@ -20,9 +20,10 @@ import src.threading as threading
 # Third-party modules
 import numpy as np
 from docopt import docopt
+from multiprocessing import Pool, cpu_count
 
 
-DIST_RANGE = [5, 10]
+DIST_RANGE = [5, 15]
 
 if __name__ == "__main__":
 
@@ -47,8 +48,16 @@ if __name__ == "__main__":
     # Parse DOPE file
     dope_df = parse.dope(DOPE)
 
+
+    scores = {}
+
     for ali in alignment_list:
-        print(ali.template.pdb)
-        matrix = threading.calc_dist_matrix(
-            ali.query_residues, ali.template.residues, DIST_RANGE)
-    # threading.display_matrix(matrix)
+        #print(ali.template.pdb)
+        matrix, dist_dict = threading.calc_dist_matrix(
+            ali.query_residues[:30], ali.template.residues[:30], DIST_RANGE)
+        #threading.display_matrix(matrix)
+
+        energy_matrix = threading.convert_dist_to_energy(matrix, dist_dict, dope_df)
+        scores[ali.template.name] = np.sum(energy_matrix)
+
+    print(max(scores))
