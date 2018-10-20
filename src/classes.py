9e5f3a984fb0ed9bc8c1524135e93c845b6e2aad
Gabriel Cretin <gabrielcretin@gmail.com>
2018-10-20 17:44:35 +0200

    Parse PDBs manually: huge gain in time

--- src/classes.py
+++ src/classes.py
@@ -5,7 +5,8 @@
 
 # Third-party modules
 from subprocess import call
-from Bio.PDB import PDBParser
+import numpy as np
+from Bio.Data.IUPACData import protein_letters_3to1
 
 
 class Alignment:
@@ -70,32 +71,29 @@ class Template:
             Returns:
                 void
         """
-        pdb = PDBParser(QUIET=True)  # QUIET = True : Warnings issued are suppressed
-
-        try:
-            structure = pdb.get_structure(self.pdb, "data/pdb/" + self.name + "/" + self.pdb)
-        except TypeError:
-            print("Silent Warning: The PDB file \"" +
-                  self.pdb + "\" has no RESOLUTION field.")
-            call(["sed -i 's/^.*RESOLUTION.*$//' data/pdb/" +
-                  self.name + "/" + self.pdb], shell=True)
-            structure = pdb.get_structure(self.pdb, "data/pdb/" + self.name + "/" + self.pdb)
         res_num = 0
-        for atom in structure.get_atoms():
-            if atom.name == "CA":
-                if res_num >= len(self.residues):
-                    break
-                # No coordinates for gaps in the template
-                while self.residues[res_num].name == '-':
-                    res_num = res_num + 1
-                self.residues[res_num].ca_coords = atom.get_vector()
-                res_num = res_num + 1
+        with open("data/pdb/" + self.name + "/" + self.pdb, 'r') as f:
+            for line in f:
+                line_type = line[0:6].strip()
+                name_at = line[12:16].strip()
+                chain = line[21:22].strip()
+                if line_type == "ATOM" and name_at == "CA":
+                    name_res = str(line[17:20].strip())
+                    num_res = int(line[22:26].strip())
+                    x = float(line[30:38].strip())
+                    y = float(line[38:46].strip())
+                    z = float(line[46:54].strip())
+                    # Skip gaps in the template
+                    while self.residues[res_num].name == '-':
+                        res_num += 1
+                    self.residues[res_num].ca_coords = np.array([x, y, z])
+                    res_num += 1
 
 
 class Residue:
     """
     .. class:: Residue
-    
+
       This class groups informations about a residue.
 
     Attributes:
@@ -110,6 +108,6 @@ class Residue:
 
     def __str__(self):
         if self.ca_coords is None:
-            return "<" + self.name + "  |  " + "empty coordinates>"
+            return "<" + self.name + " | " + "empty coordinates>"
         coords = [str(coord) for coord in self.ca_coords]
-        return "<" + self.name + "  |  " + str(coords) + ">"
+        return "<" + self.name + " | " + str(coords) + ">"
