c358642f3bc1f51dd7b7b8733c6561df1ae631ee
kabhel <helene.kabbech@gmail.com>
2018-10-22 10:26:23 +0200

    Reduce variables in function

--- src/writing.py
+++ src/writing.py
@@ -36,28 +36,24 @@ def scores_and_pdb(res_path, score_list, nb_pdb, alignment_dict):
     print("\nThe program ended successfully !\nThe results are stored in " + res_path)
 
 
-def write_pdb(res_path, template_name, template_num, alignment_dict):
+def write_pdb(pdb_filename, ali):
     """
         Write a pdb file by threading the query sequence on the template CA coordinates.
 
         Args:
-            res_path (str): The path of the directory where to stock the created files.
-            template_name (str): Name of the template used.
-            template_num (int): Rank of the template used.
-            alignment_dict (dictionary): A dictionary containing Alignment objects.
+            pdb_filename (str): Name of the pdb file to create.
+            ali (object): Alignment object used.
 
         Returns:
             void
     """
-    with open(res_path+"/pdb/model_"+str(template_num)+".pdb", "w") as file:
-        query = alignment_dict[template_name].query_residues
-        template = alignment_dict[template_name].template.residues
+    with open(pdb_filename, "w") as file:
         # Extra informations on the template used to generate the pdb file
-        file.write("REMARK Threading of query sequence on the Template {:s}.\n"\
-            .format(template_name))
-        for res_num, res_q in enumerate(query):
-            res_t = template[res_num]
-            if (res_q == "-" or res_t == "-"):
+        file.write("REMARK Threading of query sequence on the {:s} template.\n"\
+            .format(ali.template.name))
+        for res_num, res_q in enumerate(ali.query_residues):
+            res_t = ali.template.residues[res_num]
+            if res_q == "-" or res_t == "-":
                 continue
             # An "ATOM" line of the created pdb file
             file.write("{:6s}{:5d} {:^4s} {:>3s}{:>2s}{:4d}{:>12.3f}{:8.3f}{:8.3f}{:6.2f}{:6.2f}{:>12s}\n"\
