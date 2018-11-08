4057403147c89a502b92d283c3278081bf914e35
kabhel <helene.kabbech@gmail.com>
2018-11-08 16:34:05 +0100

    Update plot_scores.R

--- src/plot_scores.R
+++ src/plot_scores.R
@@ -1,5 +1,4 @@
-setwd("~/MeetU/Fold_U/res/His_biosynth")
-
+#! /usr/bin/env Rscript
 
 normalization <- function(scores) {
   return((scores - min(scores)) / (max(scores) - min(scores)))
@@ -25,9 +24,21 @@ generate_sum_benchmark_list <- function(benchmark) {
   return(cbind(sum_benchmark, family_benchmark))
 }
 
+create_benchmarch_df <- function(scores, type_scores) {
+  type = cbind(scores[,c(1,2)], type_scores)
+  type = type[order(type$type_scores, decreasing = T),]
+  bench = generate_sum_benchmark_list(type$benchmark)
+  type = cbind(type, bench)
+  return(type)
+}
+
 N = 412
 
-scores = read.table("scores.out", header=T)
+args = commandArgs(trailingOnly=TRUE)
+intput = paste(args[1], "/scores.out", sep="")
+output = paste(args[1], "/benchmark_rank.png", sep="")
+
+scores = read.table(intput, header=T)
 scores = scores[-1,]
 
 # Normalization
@@ -39,70 +50,31 @@ thr_blossum_scores =  normalization(thr_scores + blossum_scores)
 ali_thr_blossum_scores =  normalization(ali_scores + thr_scores + blossum_scores)
 
 
-ali = cbind(scores[,c(1,2)], ali_scores)
-ali = ali[order(ali$ali_scores, decreasing = T),]
-bench = generate_sum_benchmark_list(ali$benchmark)
-ali = cbind(ali, bench)
-
-thr = cbind(scores[,c(1,2)], thr_scores)
-thr = thr[order(thr$thr_scores, decreasing = T),]
-bench = generate_sum_benchmark_list(thr$benchmark)
-thr = cbind(thr, bench)
-
-blossum = cbind(scores[,c(1,2)], blossum_scores)
-blossum = blossum[order(blossum$blossum_scores, decreasing = T),]
-bench = generate_sum_benchmark_list(blossum$benchmark)
-blossum = cbind(blossum, bench)
-
-ali_thr = cbind(scores[,c(1,2)], ali_thr_scores)
-ali_thr = ali_thr[order(ali_thr$ali_thr_scores, decreasing = T),]
-bench = generate_sum_benchmark_list(ali_thr$benchmark)
-ali_thr = cbind(ali_thr, bench)
-
-thr_blossum = cbind(scores[,c(1,2)], thr_blossum_scores)
-thr_blossum = thr_blossum[order(thr_blossum$thr_blossum_scores, decreasing = T),]
-bench = generate_sum_benchmark_list(thr_blossum$benchmark)
-thr_blossum = cbind(thr_blossum, bench)
-
-ali_thr_blossum = cbind(scores[,c(1,2)], ali_thr_blossum_scores)
-ali_thr_blossum = ali_thr_blossum[order(ali_thr_blossum$ali_thr_blossum_scores, decreasing = T),]
-bench = generate_sum_benchmark_list(ali_thr_blossum$benchmark)
-ali_thr_blossum = cbind(ali_thr_blossum, bench)
+ali = create_benchmarch_df(scores, ali_scores)
+thr = create_benchmarch_df(scores, thr_scores)
+blossum = create_benchmarch_df(scores, blossum_scores)
+ali_thr = create_benchmarch_df(scores, ali_thr_scores)
+thr_blossum = create_benchmarch_df(scores, thr_blossum_scores)
+ali_thr_blossum = create_benchmarch_df(scores, ali_thr_blossum_scores)
 
 
-png(filename="benchmark_rank.png", height=700, width=700)
-par(mgp=c(2.5,1,0))
+png(filename="output", height=700, width=700)
+  par(mgp=c(2.5,1,0))
 
+  colors = c("red", "orange", "green", "cyan", "blue", "purple")
+  type_scores = list(ali, thr, blossum, ali_thr, thr_blossum, ali_thr_blossum)
 
-colors = c("red", "orange", "green", "cyan", "blue", "purple")
+  # alignment score
+  plot(type_scores[[1]]$sum_benchmark, col=colors[1],type='s', lwd=3, cex.lab=2.2, cex.axis=2, xlab="Rank", ylab="Benchmark")
+  points(type_scores[[1]]$family_benchmark, pch=4, col=colors[1], cex=2.5, lwd=2)
+  abline(b=ali$sum_benchmark[N]/N, a=0, lwd=3)
 
-# alignment score
-plot(ali$sum_benchmark, col=colors[1],type='s', lwd=3, cex.lab=2.2, cex.axis=2, xlab="Rank", ylab="Benchmark")
-points(ali$family_benchmark, pch=4, col=colors[1], cex=2.5, lwd=2)
+  for (i in 2:6) {
+    lines(type_scores[[i]]$sum_benchmark, col=colors[i], type="s", lwd=3)
+    points(type_scores[[i]]$family_benchmark, pch=4, col=colors[i], cex=2.5, lwd=2)
+  }
 
-# threading score
-lines(thr$sum_benchmark, col=colors[2], type="s", lwd=3)
-points(thr$family_benchmark, pch=4, col=colors[2], cex=2.5, lwd=2)
-
-# blossum score
-lines(blossum$sum_benchmark, col=colors[3], type="s", lwd=3)
-points(blossum$family_benchmark, pch=4, col=colors[3], cex=2.5, lwd=2)
-
-# alignment + threading
-lines(ali_thr$sum_benchmark, col=colors[4], type="s", lwd=3)
-points(ali_thr$family_benchmark, pch=4, col=colors[4], cex=2.5, lwd=2)
-
-
-# threading + blossum scores
-lines(thr_blossum$sum_benchmark, col=colors[5], type="s", lwd=3)
-points(thr_blossum$family_benchmark, pch=4, col=colors[5], cex=2.5, lwd=2)
-
-# alignment + threading + blossum scores
-lines(ali_thr_blossum$sum_benchmark, col=colors[6], type="s", lwd=3)
-points(ali_thr_blossum$family_benchmark, pch=4, col=colors[6], cex=2.5, lwd=2)
-
-
-abline(b=ali$sum_benchmark[N]/N, a=0, lwd=3)
-legend("bottomright", cex=1.2, c("alignment score", "threading score", "blossum score", "alignment + threading scores", "threading + blossum scores", "alignment + threading + blossum scores"), lwd=3, col=colors, pch=4)
-
+  legend("bottomright", cex=1.2, lwd=3, col=colors, pch=4,
+        c("alignment score", "threading score", "blossum score", "alignment + threading scores",
+          "threading + blossum scores", "alignment + threading + blossum scores"))
 dev.off()
