e521b5c02a7e830f08aae1555b3c8c4278e6764e
kabhel <helene.kabbech@gmail.com>
2018-11-09 01:04:02 +0100

    Remove normalization and beautify code

--- script/plot_scores.R
+++ script/plot_scores.R
@@ -1,17 +1,18 @@
 #! /usr/bin/env Rscript
 
-normalization <- function(scores) {
-  return((scores - min(scores)) / (max(scores) - min(scores)))
-}
+### FUNCTIONS ###
+#################
 
 generate_sum_benchmark_list <- function(benchmark) {
   sum = 0
   sum_benchmark = c()
   family_benchmark = c()
   for (b in benchmark) {
-    if (b != "."){
+    # A benchmark is found
+    if (b != 0){
       sum = sum + 1
-      if (b == "Family" | b=="Superfamily") {
+      # A family or superfamily benchmark is found
+      if (b == 2 | b==3) {
         family_benchmark = c(family_benchmark, sum)
       } else {
         family_benchmark = c(family_benchmark, -10)
@@ -25,71 +26,69 @@ generate_sum_benchmark_list <- function(benchmark) {
 }
 
 create_benchmarch_df <- function(scores, type_scores) {
-  type = cbind(scores[,c(1,2)], type_scores)
-  type = type[order(type$type_scores, decreasing = T),]
+  type = scores[,c("benchmark", type_scores)]
+  type = type[order(type[,type_scores], decreasing = T),]
   bench = generate_sum_benchmark_list(type$benchmark)
   type = cbind(type, bench)
   return(type)
 }
 
+generate_alea_benchmark <- function(N, total_bench) {
+  aleas = cbind()
+  n = 1
+  for (i in 1:n) {
+    alea = rep(0, N)
+    alea[sample(1:N, total_bench, replace=F)] = 1
+    sum = 0
+    sum_benchmark = c()
+    for (b in alea) {
+      if (b == 1) {
+        sum = sum + 1
+      }
+      sum_benchmark = c(sum_benchmark, sum)
+    }
+   aleas = cbind(aleas,sum_benchmark)
+  }
+  sum_benchmark = rowSums(aleas)/n
+  return(cbind(scores[,"benchmark"], sum_benchmark))
+}
+
+
+### MAIN ###
+############
+
+# Number of templates
 N = 412
 
 # Arguments managment
 args = commandArgs(trailingOnly=TRUE)
-input = paste(args[1], "/scores.out", sep="")
+input = paste(args[1], "/scores.csv", sep="")
 output = paste(args[1], "/benchmark_rank.png", sep="")
 
-# Reading of the scores.out file
-scores = read.table(input, header=T)
-scores = scores[-1,]
-
-# Normalization of the scores using min-max scaling method
-ali_scores = normalization(scores$alignment)
-thr_scores = normalization(scores$threading)
-blosum_scores  = normalization(scores$blosum)
-ali_thr_scores = normalization(ali_scores + thr_scores)
-thr_blosum_scores =  normalization(thr_scores + blosum_scores)
-ali_thr_blosum_scores =  normalization(ali_scores + thr_scores + blosum_scores)
+# Read the scores.out file
+scores = read.csv(input, header=T)
 
 # Number of benchmark cumulated
-ali = create_benchmarch_df(scores, ali_scores)
-thr = create_benchmarch_df(scores, thr_scores)
-blosum = create_benchmarch_df(scores, blosum_scores)
-ali_thr = create_benchmarch_df(scores, ali_thr_scores)
-thr_blosum = create_benchmarch_df(scores, thr_blosum_scores)
-ali_thr_blosum = create_benchmarch_df(scores, ali_thr_blosum_scores)
+ali = create_benchmarch_df(scores, "alignment")
+thr = create_benchmarch_df(scores, "threading")
+blosum = create_benchmarch_df(scores, "blosum")
+scores_sum = create_benchmarch_df(scores, "sum.scores")
 
 # Generate n benchmarks randomly along the ranking
-aleas = cbind()
-n = 1
-for (i in 1:n) {
-  alea = rep(0, N)
-  alea[sample(1:N, ali$sum_benchmark[N], replace=F)] = 1
-  sum = 0
-  sum_benchmark = c()
-  for (b in alea) {
-    if (b == 1) {
-      sum = sum + 1
-    }
-    sum_benchmark = c(sum_benchmark, sum)
-  }
- aleas = cbind(aleas,sum_benchmark)
-}
-sum_benchmark = rowSums(aleas)/n
-alea = cbind(scores[,c(1,2)], sum_benchmark)
+alea = generate_alea_benchmark(N, ali$sum_benchmark[N])
 
 # Create a plot with the rank in x axis and the number of benchmarks in y axis.
 # A line representes the benchmarks (family, superfamily and fold) for the different scores generated
 # A cross representes the family and superfamily benchmarks
 
 colors = c("black", "red", "orange", "green", "purple")
-type_scores = list(alea, ali, thr, blosum, ali_thr_blosum)
+type_scores = list(alea, ali, thr, blosum, scores_sum)
 
 png(filename=output, height=700, width=700)
   par(mgp=c(2.5,1,0))
 
   # alignment score
-  plot(type_scores[[1]]$sum_benchmark, col=colors[1],type='s',
+  plot(alea[,"sum_benchmark"], col=colors[1],type='s',
        lwd=3, cex.lab=2.2, cex.axis=2, xlab="Rank", ylab="Benchmark")
   for (i in 2:length(type_scores)) {
     lines(type_scores[[i]]$sum_benchmark, col=colors[i], type="s", lwd=3)
