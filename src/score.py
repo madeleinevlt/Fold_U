3dbb7c7475543f8fdbe8d03c89ad08f9351f7389
kabhel <helene.kabbech@gmail.com>
2018-11-22 11:06:18 +0100

    Mistake '/' in path

--- src/score.py
+++ src/score.py
@@ -68,36 +68,11 @@ class Score:
         # Sort of the templates according to the sum score
         scores_df = scores_df.sort_values(by="sum scores", ascending=False)
         # A csv file containing the normalized scores is created
-        scores_df.to_csv(res_path+"scores.csv")
+        print(scores_df)
+        print(res_path)
+        scores_df.to_csv(res_path+"/scores.csv")
 
         # Only nb_pdb pdb files are created
         for i in range(nb_pdb):
             pdb_filename = res_path + "/pdb/top_" + str(i+1) + ".pdb"
             alignment_dict[scores_df.index[i]].write_pdb(pdb_filename)
-
-
-
-    def write_score(self, res_path, nb_pdb, alignment_dict):
-        """
-            Creates a scores.out file containing for each line a score and the template's
-            name associated. Creates the pdb files of the n best templates (best scores).
-            Args:
-                res_path (str): The path of the directory where to stock the created files.
-                nb_pdb (int): Number of pdb to create using the n first templates.
-                alignment_dict (dictionary): A dictionary containing Alignment objects.
-        """
-        os.makedirs(res_path+"/pdb", exist_ok=True)
-        with open(res_path+"/scores.out", "w") as file:
-            template_num = 1
-            file.write("{:<18s} {:<11s} {:<9s} {:<9s} {:<9s}\n"\
-                .format("template_name", "benchmark", "alignment", "threading", "blosum"))
-            for _, ali_score, thr_score, blosum_score, name, benchmark in sorted(self.iterator):
-                # Write a line in the score.out file containing the
-                # score and the name of the current ranked template
-                file.write("{:<18s} {:<11s} {:<9d} {:<9d} {:<9d}\n"\
-                    .format(name, benchmark, int(ali_score), int(thr_score), int(blosum_score)))
-                # Only nb_pdb pdb files are created
-                if template_num <= nb_pdb:
-                    pdb_filename = res_path + "/pdb/top_" + str(template_num) + ".pdb"
-                    alignment_dict[name].write_pdb(pdb_filename)
-template_num += 1
