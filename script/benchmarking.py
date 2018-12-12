b3184b3cf0af66c206a8971b94082588b4235dd1
kabhel <helene.kabbech@gmail.com>
2018-12-12 15:58:43 +0100

    Fix bug plot, clean script

--- script/benchmarking.py
+++ script/benchmarking.py
@@ -46,6 +46,17 @@ from matplotlib import cycler
 from docopt import docopt
 from schema import Schema, And, Use, SchemaError
 
+# plot settings
+COLORS = cycler('color', ['#EE6666', '#3388BB', '#9988DD',
+                          '#EECC55', '#88BB44', '#FFBBBB'])
+plt.rc('axes', facecolor='#E6E6E6', edgecolor='none',
+       axisbelow=True, grid=True, prop_cycle=COLORS)
+plt.rc('grid', color='w', linestyle='solid')
+plt.rc('xtick', direction='out', color='gray')
+plt.rc('ytick', direction='out', color='gray')
+plt.rc('patch', edgecolor='#E6E6E6')
+plt.rc('lines', linewidth=1.5)
+
 
 def check_args():
     """
@@ -68,8 +79,49 @@ def check_args():
     except SchemaError as err:
         exit(err)
 
+def create_bencharming_scores_dict(SCORES, STRUCTURES, DSSP_PATH, NB_PROC):
+    # A dictionary of pandas DataFrames is created for each score
+    # Each DataFrame will contain the cumulative sum of benchmarks for each structure (= 3 columns)
+    benchmarking_scores = {}
+    min_rank = 900
+    for score in SCORES:
+        benchmarking_scores[score] = pd.DataFrame(np.zeros((405, 3)), columns=STRUCTURES)
+    # For each query,
+    ALL_FOLDRECS = os.listdir("data/foldrec")
+    print("Processing all benchmarks ...\n")
+    for ind, query in enumerate(ALL_FOLDRECS, 1):
+        query = query.split(".")[0]
+        # The Fold_U program is run on the current query if results are not already generated
+        if not os.path.isfile("results/" + query + "/scores.csv"):
+            print("\nProcessing query {} / {} : {}\n".format(ind, len(ALL_FOLDRECS), query))
+            p = subprocess.Popen(["./fold_u", "data/foldrec/" + query + ".foldrec",
+                                  "data/aln/" + query + ".fasta",
+                                  "-o", "results/" + query, "--dssp", DSSP_PATH,
+                                  "--cpu", NB_PROC],
+                                 stdout=subprocess.PIPE).communicate()[0]
+            rows, columns = os.popen('stty size', 'r').read().split()
+            print("\n" + "-"*int(columns))
+        # Score results are stored in a pandas DataFrame
+        query_scores = pd.read_csv("results/" + query + "/scores.csv", index_col=0)
+        if len(query_scores) < min_rank:
+            min_rank = len(query_scores)        
+        for score in SCORES:
+            # The DataFrame is sorted by the current score
+            query_score = query_scores.sort_values(by=score, ascending=False)
+            # Initialization of the dictionary of counts
+            structures_count = {}
+            for structure in STRUCTURES:
+                structures_count[structure] = 0
+            # Cumulative sum of benchmark according to the structure
+            for i, struct_type in enumerate(query_score["benchmark"]):
+                for structure in STRUCTURES:
+                    if struct_type == structure:
+                        structures_count[structure] += 1
+                    benchmarking_scores[score][structure][i+1] += structures_count[structure]
+            benchmarking_scores[score]["all"] = benchmarking_scores[score].apply(lambda row: row.Family + row.Superfamily + row.Fold, axis=1)                    
+    return (benchmarking_scores, min_rank)
 
-def plot_benchmark(output_path, scores, rank, benchmarking_scores, selected_score):
+def plot_benchmark(output_path, min_rank, scores, benchmarking_scores, selected_score):
     """
         Create one plot for one benchmark type for all the foldrec files.
 
@@ -78,59 +130,52 @@ def plot_benchmark(output_path, scores, rank, benchmarking_scores, selected_scor
             struct (str): One of the three following : "Family", "Superfamily",
                           "Fold".
             scores (list): A list of score name.
-            rank (list): A list of rank from 1 to N.
+            min_rank (int): The last value of the rank.
             benchmarking_scores (dict): a dictionnary containing benchmarking
                                         data for each type of score for
                                         different types of structures.
 
     """
+    rank = [i for i in range(0, min_rank)]
     os.makedirs(output_path, exist_ok=True)
+    # Plot creation
     plt.figure(num="Enrichment")  # Window's name
+    plt.ylabel("Benchmark")
+    plt.xlabel("rank")
+
     # Plot all scores
     if selected_score == "all":
-        ali = benchmarking_scores[scores[0]].values
-        thr = benchmarking_scores[scores[1]].values
-        mod = benchmarking_scores[scores[2]].values
-        ss = benchmarking_scores[scores[3]].values
-        acc = benchmarking_scores[scores[4]].values
-        co_ev = benchmarking_scores[scores[5]].values
-        sum = benchmarking_scores[scores[6]].values
+        ali_struct = benchmarking_scores[scores[0]]["all"].values[:min_rank]
+        thr_struct = benchmarking_scores[scores[1]]["all"].values[:min_rank]
+        mod_struct = benchmarking_scores[scores[2]]["all"].values[:min_rank]
+        ss_struct = benchmarking_scores[scores[3]]["all"].values[:min_rank]
+        acc_struct = benchmarking_scores[scores[4]]["all"].values[:min_rank]
+        co_ev_struct = benchmarking_scores[scores[5]]["all"].values[:min_rank]
+        sum_struct = benchmarking_scores[scores[6]]["all"].values[:min_rank]
 
-        plt.plot(rank, ali, "b", label=scores[0])
-        plt.plot(rank, thr, "#ffa201", label=scores[1])
-        plt.plot(rank, mod, "#EE82EE", label=scores[2])
-        plt.plot(rank, ss, "#00B200", label=scores[3])
-        plt.plot(rank, acc, "#7a9a91", label=scores[4])
-        plt.plot(rank, co_ev, "#660033", label=scores[5])
-        plt.plot(rank, sum, "r", label=scores[6])
-        plt.plot([0, len(ali)], [0, max(ali)], "k", label="random")
+        plt.plot(rank, ali_struct, "b", label=scores[0])
+        plt.plot(rank, thr_struct, "#ffa201", label=scores[1])
+        plt.plot(rank, mod_struct, "#EE82EE", label=scores[2])
+        plt.plot(rank, ss_struct, "#00B200", label=scores[3])
+        plt.plot(rank, acc_struct, "#7a9a91", label=scores[4])
+        plt.plot(rank, co_ev_struct, "#660033", label=scores[5])
+        plt.plot(rank, sum_struct, "r", label=scores[6])
+        plt.plot([0, len(ali_struct)], [0, max(ali_struct)], "k", label="random")
+        plt.legend(loc="lower right")
         plt.title("Enrichment plot")
-        plt.ylabel("Benchmark")
-        plt.xlabel("rank")
-        plt.legend(loc="lower right")
         plt.savefig(output_path + "/" + "all_scores_plot.png")
-    elif selected_score == "sum_scores":
-        score = benchmarking_scores[selected_score].values
-        plt.plot(rank, score, "b", label=selected_score)
-        plt.plot([0, len(score)], [0, max(score)], "k", label="random")
-        plt.title("Enrichment plot of " + selected_score + " score")
-        plt.ylabel("Benchmark")
-        plt.xlabel("rank")
-        plt.legend(loc="lower right")
-        plt.savefig(output_path + "/" + selected_score + "_plot.png")
     # Plot scores individually
     else:
-        score = benchmarking_scores[selected_score].values
-        plt.plot(rank, score, "b", label=selected_score)
-        plt.plot([0, len(score)], [0, max(score)], "k", label="random")
+        score_struct = benchmarking_scores[selected_score]["all"].values[:min_rank]
+        plt.plot(rank, score_struct, "b", label=selected_score)
+        plt.plot([0, len(score_struct)], [0, max(score_struct)], "k", label="random")
         plt.title("Enrichment plot of " + selected_score + " score")
-        plt.ylabel("Benchmark")
-        plt.xlabel("rank")
         plt.legend(loc="lower right")
         plt.savefig(output_path + "/" + selected_score + "_plot.png")
 
+    print("\nThe plot is stored in " + output_path + "\n")
 
-def top_n(structures, scores, top, benchmarking_scores):
+def print_top_n(structures, scores, top, benchmarking_scores):
     """
         Show statistics based on the benchmark.list files separately for each fold-type: "Fold",
         "Family", "Superfamily".
@@ -148,25 +193,33 @@ def top_n(structures, scores, top, benchmarking_scores):
             a str "top_results" table summarizing the top results
 
     """
-    rank = 0
-    max_rank = 0
+    rank = {}
+    max_rank = {}
     if scores == "all":
         scores = "sum_scores"
-    rank = benchmarking_scores[scores][top-1]
-    max_rank = max(benchmarking_scores[scores])
-    line1 = "top{0}\t{1}/{2}\t\t{3}/{4}\t\t{5}/{6}\n".format(top,
-                                                             rank["Family"],
-                                                             max_rank["Family"],
-                                                             rank["Superfamily"],
-                                                             max_rank["Superfamily"],
-                                                             rank["Fold"],
-                                                             max_rank["Fold"])
+    for struct in structures:
+        rank[struct] = benchmarking_scores[scores][struct][top-1]
+        max_rank[struct] = max(benchmarking_scores[scores][struct])
+    line1 = "top{0}\t{1}/{2}\t\t{3}/{4}\t\t{5}/{6}\n".format(top, rank["Family"],
+             max_rank["Family"], rank["Superfamily"], max_rank["Superfamily"], rank["Fold"],
+             max_rank["Fold"])
     line2 = "\t{0:>5.2f} % {1:>13.2f} %{2:>14.2f} %"\
         .format((rank["Family"]/max_rank["Family"])*100,
                 (rank["Superfamily"]/max_rank["Superfamily"])*100,
                 (rank["Fold"]/max_rank["Fold"])*100)
     top_results = line1 + line2
     return top_results
+
+def print_table(nb_templates, structures, selected_score, benchmarking_scores):
+    n_top_n = [5]
+    if nb_templates <= 5: n_top_n = [5]
+    elif nb_templates >= 10: n_top_n = [5, 10]
+    elif nb_templates >= 50: n_top_n = [5, 10, 50]
+    elif nb_templates >= 100: n_top_n = [5, 10, 50, 100]
+    print("Table summarizing the top {} results.\n".format(n_top_n))
+    print("\tFamily\t\tSuperfamily\tFold\n")
+    for topn in n_top_n:
+        print(print_top_n(structures, selected_score, topn, benchmarking_scores))
 
 
 if __name__ == "__main__":
@@ -192,68 +245,10 @@ if __name__ == "__main__":
     # all the possible scores useful for plots
     SCORES = ["alignment", "threading", "modeller", "secondary_structure", "solvent_access",
               "co_evolution", "sum_scores"]
-    # A dictionary of pandas DataFrames is created for each score
-    # Each DataFrame will contain the cumulative sum of benchmarks for each structure (= 3 columns)
-    BENCHMARKING_SCORES = {}
-    N = 405
-    for score in SCORES:
-        BENCHMARKING_SCORES[score] = pd.Series([0]*N)
-    # For each query,
-    ALL_FOLDRECS = os.listdir("data/foldrec")
-    print("Processing all benchmarks ...\n")
-    for ind, query in enumerate(ALL_FOLDRECS, 1):
-        query = query.split(".")[0]
-        # The Fold_U program is run on the current query if results are not already generated
-        if not os.path.isfile("results/" + query + "/scores.csv"):
-            print("\nProcessing query {} / {} : {}\n".format(ind, len(ALL_FOLDRECS), query))
-            p = subprocess.Popen(["./fold_u", "data/foldrec/" + query + ".foldrec",
-                                  "data/aln/" + query + ".fasta",
-                                  "-o", "results/" + query, "--dssp", DSSP_PATH,
-                                  "--cpu", NB_PROC],
-                                 stdout=subprocess.PIPE).communicate()[0]
-            rows, columns = os.popen('stty size', 'r').read().split()
-            print("\n" + "-"*int(columns))
-        # Score results are stored in a pandas DataFrame
-        query_scores = pd.read_csv("results/" + query + "/scores.csv", index_col=0)
-        for score in SCORES:
-            # The DataFrame is sorted by the current score
-            query_score = query_scores.sort_values(by=score, ascending=False)
-            structures_count = 0
-            # Cumulative sum of benchmark according to the structure
-            for i, struct_type in enumerate(query_score["benchmark"]):
-                print(structures_count)
-                if struct_type in STRUCTURES:
-                    structures_count += 1
-                BENCHMARKING_SCORES[score][i+1] += structures_count
-            print('\n')
 
-    RANK = [i for i in BENCHMARKING_SCORES[SCORES[0]].index]
+    (BENCHMARKING_SCORES, MIN_RANK) = create_bencharming_scores_dict(SCORES, STRUCTURES, DSSP_PATH, NB_PROC)
 
-    # plot settings
-    COLORS = cycler('color', ['#EE6666', '#3388BB', '#9988DD',
-                              '#EECC55', '#88BB44', '#FFBBBB'])
-    plt.rc('axes', facecolor='#E6E6E6', edgecolor='none',
-           axisbelow=True, grid=True, prop_cycle=COLORS)
-    plt.rc('grid', color='w', linestyle='solid')
-    plt.rc('xtick', direction='out', color='gray')
-    plt.rc('ytick', direction='out', color='gray')
-    plt.rc('patch', edgecolor='#E6E6E6')
-    plt.rc('lines', linewidth=1.5)
-    print("\nTotal runtime: {} seconds".format(str(datetime.now() - START_TIME)))
-    plot_benchmark(OUTPUT_PATH, SCORES, RANK, BENCHMARKING_SCORES, SELECTED_SCORE)
+    plot_benchmark(OUTPUT_PATH, MIN_RANK, SCORES, BENCHMARKING_SCORES, SELECTED_SCORE)
+    print_table(NB_TEMPLATES, STRUCTURES, SELECTED_SCORE, BENCHMARKING_SCORES)
     plt.show()
-    print("\nThe plots are stored in " + OUTPUT_PATH + "\n")
-    N_TOP_N = [5]
-    if NB_TEMPLATES <= 5:
-        N_TOP_N = [5]
-    if NB_TEMPLATES >= 10:
-        N_TOP_N = [5, 10]
-    if NB_TEMPLATES >= 50:
-        N_TOP_N = [5, 10, 50]
-    if NB_TEMPLATES >= 100:
-        N_TOP_N = [5, 10, 50, 100]
-    print("Table summarizing the top {} results.\n".format(N_TOP_N))
-    print("\tFamily\t\tSuperfamily\tFold\n")
-    for topn in N_TOP_N:
-        print(top_n(STRUCTURES, SELECTED_SCORE, topn, BENCHMARKING_SCORES))
-        print("\t----------------------------------------")
+    print("\nTotal runtime: {} seconds".format(str(datetime.now() - START_TIME)))
